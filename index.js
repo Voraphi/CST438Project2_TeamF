ff5fb169f9b0f1a1547265238474b12e828f4b0b
EC2 Default User <ec2-user@ip-172-31-20-47.us-east-2.compute.internal>
2020-10-02 06:03:35 +0000

    created register and login pages

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.use(session({
     resave: true,
     saveUninitialized: true
 }));
+app.set('view engine', 'ejs');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -23,20 +24,94 @@ const connection = mysql.createConnection({
     password: 'admin',
     database: 'webstoredb'
 });
-// connection.connect();
-
-app.set('view engine', 'ejs');
+ connection.connect();
 
 
-app.get('/', function(req, res) {
+
+/* Middleware */
+function isAuthenticated(req, res, next){
+    if(!req.session.authenticated) res.redirect('/login');
+    else next();
+}
+
+function checkUsername(username){
+    let stmt = 'SELECT * FROM users WHERE username=?';
+    return new Promise(function(resolve, reject){
+       connection.query(stmt, [username], function(error, results){
+           if(error) throw error;
+           resolve(results);
+       }); 
+    });
+}
+
+function checkPassword(password, hash){
+    return new Promise(function(resolve, reject){
+       bcrypt.compare(password, hash, function(error, result){
+          if(error) throw error;
+          resolve(result);
+       }); 
+    });
+}
+
+/* Home Route*/
+app.get('/', function(req, res){
     res.render('home');
 });
 
-/* Error Route*/
-app.get('*', function(req, res) {
-    res.render('error');
+/* Login Routes */
+app.get('/login', function(req, res){
+    res.render('login');
 });
 
-app.listen(process.env.PORT || 3000, function() {
+app.post('/login', async function(req, res){
+    let isUserExist   = await checkUsername(req.body.username);
+    let hashedPasswd  = isUserExist.length > 0 ? isUserExist[0].password : '';
+    let passwordMatch = await checkPassword(req.body.password, hashedPasswd);
+    if(passwordMatch){
+        req.session.authenticated = true;
+        req.session.user = isUserExist[0].username;
+        res.redirect('/welcome');
+    }
+    else{
+        res.render('login', {error: true});
+    }
+});
+
+/* Register Routes */
+app.get('/register', function(req, res){
+    res.render('register');
+});
+
+app.post('/register', function(req, res){
+    let salt = 10;
+    console.log(req.body.password,req.body.username);
+    bcrypt.hash(req.body.password, salt, function(error, hash){
+        if(error) throw error;
+        let stmt = 'INSERT INTO users (username, password, firstname, lastname) VALUES (?, ?, ?, ?)';
+        let data = [req.body.username, hash, req.body.firstname, req.body.lastname];
+        connection.query(stmt, data, function(error, result){
+           if(error) throw error;
+           res.redirect('/login');
+        });
+    });
+});
+
+/* Logout Route */
+app.get('/logout', function(req, res){
+   req.session.destroy();
+   res.redirect('/');
+});
+
+/* Welcome Route */
+app.get('/welcome', isAuthenticated, function(req, res){
+   res.render('welcome', {user: req.session.user}); 
+});
+
+/* Error Route*/
+app.get('*', function(req, res){
+   res.render('error'); 
+});
+
+app.listen(process.env.PORT || 3000, function(){
     console.log('Server has been started');
 })
